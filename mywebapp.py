083eee7b7f81f3a418d34c33a18bf9b9e7ba8443
mahadevaprabhug <mahadevaprabhu.g@gmail.com>
2020-10-06 15:32:02 +0530

    This is my first commit

---
+++ mywebapp.py
@@ -0,0 +1,11 @@
+import flask
+
+app = flask.Flask("mywebapp")
+
+@app.route("/")
+def indexpage():
+    return "Wel Come"
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ test_mywebapp.py
@@ -0,0 +1,8 @@
+'''
+Using pytest we are testing
+'''
+
+from mywebapp import indexpage
+
+def test_mytest():
+    assert indexpage() == "Wel Come"
